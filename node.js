f6ae51b67347d7ce3a3a94eccdd9d8d9f7c8d221
Lautaro <altairdiaz245@gmail.com>
2023-02-16 15:23:24 -0300

    hola

---
+++ node.js
@@ -0,0 +1,13 @@
+const { spawn } = require("node:child_process");
+const cmus = spawn("cmus-remote", ["-Q"]);
+const play = (v) => spawn("cmus-remote", [`-${v}`]);
+const tag = "tag";
+
+cmus.stdout.on("data", (d) => {
+  const cmus = d.toString().trim().split("\n");
+  const cmusStatus = cmus.find((e) => e.startsWith(`status`));
+  const estado = cmusStatus.split(" ")[1];
+  
+  if (estado == "playing") play("u");
+  else if (estado == "paused") play("p");
+});
